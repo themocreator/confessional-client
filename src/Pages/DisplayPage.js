c374109a3e48728671a1a8a74111021b05bf8015
Monea' Shepherd <moneashepherd@Moneas-MacBook-Pro-2.local>
2024-05-04 12:46:08 -0400

    adding func

--- src/Pages/DisplayPage.js
+++ src/Pages/DisplayPage.js
@@ -1,34 +1,105 @@
-import React, { useState, useRef, useCallback } from 'react'
-import Webcam from 'react-webcam'
+import React, { useState, useRef, useCallback, useEffect } from "react";
+import Webcam from "react-webcam";
+import "./DisplayPage.css";
 
 const DisplayPage = () => {
-  const [image, setImage] = useState([])
-  const webcamRef = useRef(null)
-  const capture = useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot()
-    setImage(imageSrc)
-  }, [webcamRef]);
-  
+  /* To Do 
+
+1. on message -> "photo" -> open webcam 
+2. after 4 photos -> publish "done" 
+3. on message -> send + phone number -> text api
+
+*/
+  const [images, setImages] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [active, setActive] = useState("done");
+  const [success, setSuccess] = useState(null);
+
+  const webcamRef = useRef(null);
+
+  // const capture = useCallback(() => {
+  //   const imageSrc = webcamRef.current.getScreenshot();
+  //   setImages(i => [...i, imageSrc]);
+  // }, [webcamRef]);
+
   const videoConstraints = {
     width: 1280,
     height: 720,
-    facingMode: "user"
+    facingMode: "user",
   };
 
-  return (
-    <div>
-  <Webcam
-      audio={false}
-      height={720}
-      ref={webcamRef}
-      screenshotFormat="image/jpeg"
-      width={1280}
-    videoConstraints={videoConstraints}
-    />
-      {image && <img src={image} alt={'captured-img'}/>}
-    <button onClick={capture}>Capture photo</button>
-    </div>
-  )
-}
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (active === "webcam") {
+        if (counter < 4) {
+          const imageSrc = webcamRef.current.getScreenshot();
+          setImages((i) => [...i, imageSrc]);
+          setCounter((prevCounter) => prevCounter + 1);
+        } else {
+          setSuccess(true);
+          setActive("review");
+          clearInterval(interval);
+        }
+      }
+    }, 2000);
 
-export default DisplayPage
+    return () => clearInterval(interval);
+  }, [counter, active]);
+  return (
+    <div className="default-view">
+      {active === "webcam" && (
+        <div className="webcam-view">
+          <span>{4 - counter} remaining</span>
+          <Webcam
+            audio={false}
+            height={720}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+            videoConstraints={videoConstraints}
+          />{" "}
+        </div>
+      )}
+      {active === "home" && (
+        <div className="default-view">
+          <div className="info">
+            <span className="heading">Welcome to the confessional</span>
+            <span className="heading3">How It Works</span>
+            <div className="steps">
+              <span>Select Get Started</span>
+              <span>Smile for 4 Photos</span>
+              <span>
+                Enter your phone number to get your pictures via text.
+              </span>
+            </div>
+          </div>
+          <div className="box">
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+      )}
+      {active === "done" && (
+        <div className="default-view">
+          <div className="info">
+          <span className="heading">Thank you for using The Confessional.</span>
+            <span className="heading3">Enter your phone number</span>
+            </div>
+          <div className="box">
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+      )}
+      {/* {images.map((i) => {
+        return <img src={i} alt='captured-photo' />
+      })} */}
+      {/* {image && <img src={image} alt={'captured-img'}/>} */}
+      {/* <button onClick={() => setActiveCamera(true)}>Capture photo</button> */}
+    </div>
+  );
+};
+
+export default DisplayPage;
--- src/Pages/TabletPage.js
+++ src/Pages/TabletPage.js
@@ -1,42 +1,80 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import mqtt from "mqtt";
-import './TabletPage.css'
+import "./TabletPage.css";
 
 const TabletPage = () => {
+  /* To Do 
 
-  // const takePhoto = async () => {
-  //   return await fetch("http://localhost:8080/takePhoto", {
-  //     mode: "no-cors",
-  //   });
-  // };
+1. on click -> sendMessage(photo)
+2. on message -> done -> get number -> publish(number)
 
-  // const recordBoomerang = async () => {
-  //   return await fetch("http://localhost:8080/takeBoomerang", {
-  //     mode: "no-cors",
-  //   });
-  // };
+*/
+  const sendText = async () => {
+    return await fetch("https://mixingmos-api.vercel.app/textPhotosVideos", {
+      method: 'POST',
+      body: JSON.stringify({
+        phone: '+18137149450'
+      })
+    }).then((res) => res.json())
+  }
+  //   return await fetch("https://mixingmos-api.vercel.app/textPhotosVideos", {
+  //     method: "POST", // *GET, POST, PUT, DELETE, etc.
+  //     mode: "no-cors", 
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       // 'Content-Type': 'application/x-www-form-urlencoded',
+  //     },
+  //     body: JSON.stringify({
+  //       phone: '+18137149450'
+  //     })
+  //   }).then((res) => res.json())
+  // }
+  const [active, setActive] = useState("phone");
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  const handlePhoneNumberChange = (e) => {
+    const inputValue = e.target.value;
+    if (inputValue.length < 11) {
+      setPhoneNumber(inputValue);
+    }
+   
+    // Regular expression for validating phone numbers in the format (XXX) XXX-XXXX
+    const phonePattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    setIsValid(phonePattern.test(inputValue));
+  };
+
   useEffect(() => {
-      const client = mqtt.connect('wss://192.168.0.170:8080')
-        // Subscribe to a topic
-        client.on('connect', () => {
-          console.log('Connected to MQTT broker');
-          client.subscribe('confessional');
-        });
-    
-        // Handle incoming messages
-        client.on('message', (topic, payload) => {
-          console.log('Received message:', payload.toString());
-          // Update React state or trigger actions based on the received message
-         console.log(payload.toString());
-        });
-  })
-  
+    const client = mqtt.connect("wss://192.168.0.170:8080");
+    // Subscribe to a topic
+    client.on("connect", () => {
+      console.log("Connected to MQTT broker");
+      client.subscribe("confessional");
+    });
+
+    client.on('error', (err) => {
+      console.error('Connection error: ', err);
+      client.end();
+    });
+
+    // Handle incoming messages
+    client.on("message", (topic, payload) => {
+      console.log("Received message:", payload.toString());
+      // Update React state or trigger actions based on the received message
+      setActive(payload.toString());
+    });
+  });
+
   return (
-      <div className="tablet-container">
-      <div className="btn-group">
-              <button>Take a pic</button>
-              <button>Record a video</button>
-      </div>
+    <div className="tablet-container">
+    { active === "start" && <div className="btn-group">
+        <button>Get Started</button>
+      </div>}
+      {active === "phone" && <div className="tablet-container">
+        <span className="opt">By providing your phone number, you're opting in to receive text messages.</span>
+        <input className="phone-input" type='tel' value={phoneNumber} onChange={handlePhoneNumberChange} />
+        {isValid && <button className="primary" onClick={sendText}>Send Photos</button>}
+      </div>}
     </div>
   );
 };
